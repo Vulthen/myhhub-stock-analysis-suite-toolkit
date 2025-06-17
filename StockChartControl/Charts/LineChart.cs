c51d4d38989831d6cd65d35afb419b6a67143a11
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:24 +0300

    LineChart.cs

---
+++ StockChartControl/Charts/LineChart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using StockChartControl.Model;
+
+namespace StockChartControl.Charts
+{
+    class LineChart : IChartDrawing
+    {
+        public void Draw(DrawingContext dc, List<Point> filteredPoints, ChartStyle chartStyle)
+        {
+            var geometry = new StreamGeometry();
+            using (StreamGeometryContext context = geometry.Open())
+            {
+                context.BeginFigure(filteredPoints[0], false, false);
+                context.PolyLineTo(filteredPoints, true, true);
+            }
+            geometry.Freeze();
+
+            Pen pen = new Pen(chartStyle.LineColor, 1);
+
+            dc.DrawGeometry(null, pen, geometry);
+        }
+    }
+}
