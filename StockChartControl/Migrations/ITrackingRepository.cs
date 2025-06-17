3b74072bde70da56f1bdda21042e9e02e5318868
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:54 +0300

    ITrackingRepository.cs

---
+++ StockChartControl/Migrations/ITrackingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutomaticStockTrader.Repository
+{
+    public interface ITrackingRepository : IDisposable
+    {
+        public Task AddPendingOrder(Domain.StrategysStock postion, Domain.Order order);
+        public Task CompleteOrder(Domain.Order completedOrder);
+        public Task AddOrder(Domain.StrategysStock strategysStock, Domain.Order order);
+        public Task<Domain.Position> GetOrCreateEmptyPosition(Domain.StrategysStock strategysStock);
+        public IEnumerable<Domain.Order> GetCompletedOrders(Domain.StrategysStock strategysStock);
+    }
+}
