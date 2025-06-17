7b2b2b31e497b3c2abd8b90af41bc31bc3b6ccc1
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:12 +0300

    ReadTushareStockMarketThread.cs

---
+++ StockChartControl/ReadTushareStockMarketThread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock.Market
+{
+    public class ReadTushareStockMarketThread : ReadStockMarketThread
+    {
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
