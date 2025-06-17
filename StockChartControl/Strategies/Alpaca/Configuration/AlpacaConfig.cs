aaf16180346b5e9c8ce78d62e167bf05a9ee5cdc
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:03 +0300

    AlpacaConfig.cs

---
+++ StockChartControl/Strategies/Alpaca/Configuration/AlpacaConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomaticStockTrader.Core.Configuration
+{
+    public class AlpacaConfig
+    {
+        public bool Alpaca_Use_Live_Api { get; set; }
+        public string Alpaca_Secret_Key { get; set; }
+        public string Alpaca_App_Id { get; set; }
+    }
+}
