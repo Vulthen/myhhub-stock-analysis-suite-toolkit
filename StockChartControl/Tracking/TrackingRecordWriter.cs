48b20cb6656453c6c09fa1d00417c4c1c18ece6f
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:12:29 +0300

    TrackingRecordWriter.cs

---
+++ StockChartControl/Tracking/TrackingRecordWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace AutomaticStockTrader.Tests.Tracking
+{
+    public static class TrackingRecordWriter
+    {
+        private const string COLLECTION = "tracking";
+
+        public static async Task WriteData(TrackingConfig config, double percentageMade, string strategyName)
+        {
+            Debug.Write($"Google app creds: {config.Google_Application_Credentials}");
+            if (!string.IsNullOrWhiteSpace(config.Google_Application_Credentials))
+            {
+                await (await FirestoreDb.CreateAsync("automatic-stock-trader-tracker")).Collection(COLLECTION).AddAsync(new { DateTime.UtcNow.Date, percentageMade, strategyName });
+            }
+        }
+    }
+}
