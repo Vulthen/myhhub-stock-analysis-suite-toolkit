c926276d2a4a25b5300178916b170f306f6cc5ca
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:11:55 +0300

    IStrategy.cs

---
+++ StockChartControl/Strategies/IStrategy.cs
@@ -0,0 +1,11 @@
+﻿using AutomaticStockTrader.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutomaticStockTrader.Core.Strategies
+{
+    public interface IStrategy
+    {
+        Task<bool?> ShouldBuyStock(IList<StockInput> HistoricalData);
+    }
+}
