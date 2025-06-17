c8fbcf0d806ef262a296dfacc8e41866401f81d6
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:11:01 +0300

    Order.cs

---
+++ StockChartControl/Migrations/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutomaticStockTrader.Repository.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid PositionId { get; set; }
+
+        public DateTimeOffset OrderPlaced { get; set; }
+
+        public decimal AttemptedSharesBought { get; set; }
+
+        public decimal AttemptedCostPerShare { get; set; }
+
+        public decimal? ActualSharesBought { get; set; }
+
+        public decimal? ActualCostPerShare { get; set; }
+
+        public virtual StratagysStock Position { get; set; }
+    }
+}
